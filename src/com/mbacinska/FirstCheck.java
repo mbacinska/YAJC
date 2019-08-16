ce8573927af4df6a56c5d4115ad674eb213f1fbe
m.bacinska <m.bacinska@samsung.com>
2019-08-16 18:42:58 +0200

    Code refactor at home

--- src/com/mbacinska/FirstCheck.java
+++ src/com/mbacinska/FirstCheck.java
@@ -7,6 +7,17 @@ public class FirstCheck {
         System.out.println("Yet Another Camp in Java. Welcome!");
         System.out.println("Max integer number is:" +Integer.MAX_VALUE);
         System.out.println("Min integer number is:" +Integer.MIN_VALUE);
+        int overflowInt = Integer.MAX_VALUE+1000;
+        System.out.println("Overflow: " +overflowInt);
+        int underflowInt = Integer.MIN_VALUE - 256784;
+        System.out.println("Underflow: "+underflowInt);
+        byte byteVarMax = Byte.MAX_VALUE;
+        System.out.println("Byte max value is: "+byteVarMax);
+        byteVarMax-=3;
+        System.out.println("Byte max value -3 equals:"+byteVarMax);
+        byteVarMax += 10;
+        System.out.println("Byte max value overflowed :"+byteVarMax);
+
         System.out.println(0.0/0.0); //tylko dla l.zmiennoprzecinkowych
        // System.out.println(0/0);  //error
         System.out.println(-0.0<+0.0);
--- src/com/mbacinska/Variable.java
+++ src/com/mbacinska/Variable.java
@@ -3,12 +3,13 @@ package com.mbacinska;
 public class Variable {
 
     public static void main(String[] args) {
-        int a = 10; //inicializujmy zmienna jednoczesnie z deklaracja
+        int a = 10; //inicializujmy zmienna jednoczesnie z deklaracja;nazwa powinna być znacząca
         System.out.println(a);
         int b = 20, c = 30; //tak nie robimy
         int d = 40;
         d = d + 3;
         d += 3;
+        System.out.println("d: "+d);
     }
 
 }

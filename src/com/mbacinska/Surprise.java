126b4724adae04955cecac50ba20d20214a371c6
Monika Bacinska <monika.bacinska@onet.pl>
2019-08-24 12:37:46 +0200

    Lesson02

---
+++ src/com/mbacinska/Surprise.java
@@ -0,0 +1,35 @@
+package com.mbacinska;
+
+
+public class Surprise {
+
+
+    public static boolean first() {
+        System.out.println("first");
+        return false;
+    }
+
+    public static boolean second() {
+        System.out.println("second");
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(first() && second()); //jesli najpierw jest false, to drugiego operanda juz nie sprawdza, bo calosc jest false
+
+        boolean lhs = false;
+        boolean rhs = true;
+        boolean result = isResult(lhs, rhs);
+        System.out.println(result);
+        
+        System.out.println(1500_100_900);
+        System.out.println(123_45_55_9876L);
+    }
+
+    private static boolean isResult(boolean lhs, boolean rhs) {
+        return lhs && rhs;
+    }
+
+
+}

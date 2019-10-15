8c028d22ec85eeb4060d19f666104adfdfaccf8a
m.bacinska <m.bacinska@samsung.com>
2019-10-15 16:36:21 +0200

    Lesson10

--- src/org/mb/lambda/NotSoSimple.java
+++ src/org/mb/lambda/NotSoSimple.java
@@ -1,6 +1,31 @@
 package org.mb.lambda;
 
+import java.util.function.Supplier;
+
 public class NotSoSimple {
 
-    
+    static class SomeClass{
+        public SomeClass() {
+            System.out.println("constructor");
+        }
+    }
+
+    public static void main(String[] args) {
+
+//        Runnable r = new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("go");
+//            }
+//        };
+        Runnable r = () -> System.out.println("go");
+
+        Supplier<Object> someObject = Object::new;//anonimowy akcesor do metory new w klasie Object
+        someObject.get();
+        //new Object(); zastepuje dwie powyzsze linijki
+
+        Supplier<SomeClass> someNewObject = () ->new SomeClass();
+        someNewObject.get();
+
+    }
 }

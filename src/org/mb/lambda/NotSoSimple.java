452e760df11063342b36e34a0c53b58d750bd2ce
m.bacinska <m.bacinska@samsung.com>
2019-10-15 16:43:07 +0200

    Lesson10

--- src/org/mb/lambda/NotSoSimple.java
+++ src/org/mb/lambda/NotSoSimple.java
@@ -1,14 +1,9 @@
 package org.mb.lambda;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class NotSoSimple {
-
-    static class SomeClass{
-        public SomeClass() {
-            System.out.println("constructor");
-        }
-    }
 
     public static void main(String[] args) {
 
@@ -24,8 +19,21 @@ public class NotSoSimple {
         someObject.get();
         //new Object(); zastepuje dwie powyzsze linijki
 
-        Supplier<SomeClass> someNewObject = () ->new SomeClass();
+        Supplier<SomeClass> someNewObject = () -> new SomeClass();
         someNewObject.get();
 
+        Function<Integer, Integer> f = (i1) -> {
+            System.out.println("In function");
+            return i1 + 1500;
+        };
+        System.out.println(f.apply(200));
+
     }
+    static class SomeClass {
+        public SomeClass() {
+            System.out.println("constructor");
+        }
+    }
+
+
 }
